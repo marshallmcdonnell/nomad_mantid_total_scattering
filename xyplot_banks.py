f067c9b7fcdd4087fafe249b584a2a00816d083b
McDonnell, Marshall T <mcdonnellmt@ornl.gov>
2017-07-17 10:53:30 -0400

    Updated xyplot_banks.

--- xyplot_banks.py
+++ xyplot_banks.py
@@ -31,13 +31,17 @@ def plot_banks(f, title=None,banks_list=None,line_style=None):
     banks_list = [ int(i) for i in banks_list ]
 
 
-    colors = [ 'r', 'b', 'g', 'k', 'y', 'c' ]
+    color_line_styles = { 0 : 'k',
+                1 : 'r',
+                2 : 'b',
+                3 : 'g',
+                4 : 'y',
+                5 : 'c' }
 
     for i in range(nsets):
         if i in banks_list:
             myLabel = 'bank: '+str(i)+' '+title.split('.')[0]
-            #plt.plot(datasets[i,:,0], datasets[i,:,1],colors[i]+line_style,label=myLabel )
-            plt.plot(datasets[i,:,0], datasets[i,:,1],line_style,label=myLabel )
+            plt.plot(datasets[i,:,0], datasets[i,:,1],line_style+color_line_styles[i],label=myLabel )
 
     
 
@@ -47,29 +51,25 @@ parser.add_argument('-b', '--banks', nargs='+', help='Banks to plot')
 args = parser.parse_args()
 print args
 
-banks_line_styles = { 0 : '-',
+line_styles = { 0 : '-',
                 1 : '--',
                 2 : ':',
                 3 : '-x',
                 4 : '--x',
                 5 : ':x' }
 
-color_line_styles = { 0 : '-k',
-                1 : '-r',
-                2 : '-b',
-                3 : '-g',
-                4 : '-y',
-                5 : '-c' }
-
 
 
 
 for i, filename in enumerate(args.filenames):
     with open(filename, 'r') as f:
-        plot_banks(f,title=filename,banks_list=args.banks, line_style=color_line_styles[i])
+        if len(args.filenames) == 1:
+            plot_banks(f,title=filename,banks_list=args.banks, line_style=line_styles[i])
+        else:
+            plot_banks(f,title=filename,banks_list=args.banks, line_style=line_styles[i])
 
 plt.legend()
 plt.xlabel("Q (angstroms^-1")
 plt.ylabel("I(Q) (arb. units)")
-plt.title("Carpenter Corrections (Abs. + Mult. Scat.)")
+#lt.title("Carpenter Corrections (Abs. + Mult. Scat.)")
 plt.show()            
