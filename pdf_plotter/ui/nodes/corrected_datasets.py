ec41501d36d713d9907c82b82406babeea42640c
ntm <ntm@ornl.gov>
2017-10-11 12:26:31 -0400

    Added x-range view to CorrectedDatasetsNodeControls.

--- pdf_plotter/ui/nodes/corrected_datasets.py
+++ pdf_plotter/ui/nodes/corrected_datasets.py
@@ -4,7 +4,8 @@ from traits.api \
     import Button
 
 from traitsui.api \
-    import View, HGroup, HSplit, Item, CheckListEditor
+    import View, HGroup, HSplit, VGroup, VSplit, Item, \
+    CheckListEditor, TextEditor, RangeEditor
 
 from ui.nodes.base_node \
     import NodeButtonHandler, NodeButtons, NodeControls
@@ -47,12 +48,70 @@ class CorrectedDatasetsNodeButtons(NodeButtons):
 
 class CorrectedDatasetsNodeControls(NodeControls):
     traits_view = View(
-        HSplit(
-            Item('selected_cmap',
-                 editor=CheckListEditor(name='cmap_list'),
-                 show_label=False,
-                 ),
-            show_border=True,
-            label='ColorMap',
+        VGroup(
+            HSplit(
+                Item('selected_cmap',
+                     editor=CheckListEditor(name='cmap_list'),
+                     show_label=False,
+                     ),
+                show_border=True,
+                label='ColorMap',
+            ),
+
+            # X range
+            VSplit(
+
+                # Xmin
+                HSplit(
+                    Item('xmin_min',
+                         width=0.1,
+                         editor=TextEditor(auto_set=False,),
+                         label='Min',
+                         ),
+                    Item('xmin',
+                         editor=RangeEditor(
+                             mode='slider',
+                             low_name='xmin_min',
+                             high_name='xmin_max',
+                             format='%4.2f',
+                         ),
+                         width=0.8,
+                         show_label=False,
+                         ),
+                    Item('xmin_max',
+                         width=0.1,
+                         editor=TextEditor(auto_set=False,),
+                         label='Max',
+                         ),
+                    label='Xmin',
+                ),
+
+                # Xmax
+                HSplit(
+                    Item('xmax_min',
+                         width=0.1,
+                         editor=TextEditor(auto_set=False,),
+                         label='Min',
+                         ),
+                    Item('xmax',
+                         editor=RangeEditor(
+                             mode='slider',
+                             low_name='xmax_min',
+                             high_name='xmax_max',
+                             format='%4.2f',
+                         ),
+                         width=0.8,
+                         show_label=False,
+                         ),
+                    Item('xmax_max',
+                         width=0.1,
+                         editor=TextEditor(auto_set=False,),
+                         label='Max',
+                         ),
+                    label='Xmax',
+                ),
+                show_border=True,
+                label='X-range',
+            ),
         ),
     )
